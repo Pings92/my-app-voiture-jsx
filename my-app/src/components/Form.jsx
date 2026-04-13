9d00b42362590c206db0ee372ce20ff54591ac56
pierickj <pierick.jacquet@gmail>
2026-04-13 15:14:29 +0200

    new color added

--- my-app/src/components/Form.jsx
+++ my-app/src/components/Form.jsx
@@ -17,7 +17,21 @@ class Form extends Component {
                 "lightsalmon",  "darkkhaki",    "lime", "darkgoldenrod",    "green",
                 "cornsilk", "blanchedalmond",   "sandybrown",   "yellow",   "mediumseagreen",
                 "olivedrab",    "forestgreen",  "ivory",    "papayawhip",   "burlywood",
-                "yellowgreen",  "springgreen",  "seagreen", "darkslategray",]
+                "yellowgreen",  "springgreen",  "seagreen", "darkslategray","beige", "moccasin",
+                 "tan", "chartreuse", "mediumspringgreen", "lightseagreen", "teal", "lightyellow",
+                  "wheat", "khaki", "lawngreen", "aqua", "darkturquoise", "darkcyan",
+                   "lightgoldenrodyellow", "lemonchiffon", "greenyellow", "darkseagreen",
+                    "cyan", "deepskyblue", "midnightblue", "honeydew", "palegoldenrod",
+                     "lightgreen", "mediumaquamarine", "cadetblue", "steelblue", "navy",
+                      "mintcream", "palegreen", "skyblue", "turquoise", "dodgerblue", "blue",
+                       "darkblue", "azure", "aquamarine", "lightskyblue", "mediumturquoise",
+                        "lightslategray", "blueviolet", "mediumblue", "lightcyan", "paleturquoise",
+                         "lightsteelblue", "cornflowerblue", "slategray", "darkorchid", "darkslateblue",
+                          "aliceblue", "powderblue", "thistle", "mediumslateblue", "royalblue",
+                           "darkviolet", "rebeccapurple", "ghostwhite", "lightblue", "plum", "mediumpurple",
+                            "slateblue", "magenta", "indigo", "lavender", "pink", "violet", "orchid",
+                             "mediumorchid", "mediumvioletred", "purple", "lavenderblush", "lightpink",
+                              "hotpink", "palevioletred", "deeppink", "crimson", "darkmagenta"]
         
     }
     handlePseudo = (e) => {
